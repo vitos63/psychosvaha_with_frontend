dca332ed92d0f48949ed9fd2ea495e16d915e25e
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-01-25 12:19:48 +0300

    ✨ (frontend): Add user data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ClientFormComponent from './components/ClientFormComponents/ClientFormComponent';
@@ -9,25 +9,40 @@ import TherapistSecondFormComponent from './components/TherapisFormComponents/Th
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
    useEffect(() => {
         if (window.Telegram && window.Telegram.WebApp) {
             const tg = window.Telegram.WebApp;
 
             tg.ready();
             tg.expand();
-            const user = tg.initDataUnsafe?.user;
+            const userData = tg.initDataUnsafe?.user;
             
-            if (user) {
-                console.log('Telegram ID:', user.id);
-                console.log('Username:', user.username);
-                console.log('Имя:', user.first_name);
-                console.log('Фамилия:', user.last_name);
-            }
+            if (userData) {
+              setUser(userData);
+              console.log('Telegram ID:', userData.id);
+              console.log('Username:', userData.username);
+              console.log('Имя:', userData.first_name);
+              console.log('Фамилия:', userData.last_name);
+      }
         }
     }, []);
 
   return (
     <Router>
+      {user && (
+        <div style={{
+          padding: '10px',
+          background: '#f0f0f0',
+          marginBottom: '20px'
+        }}>
+          <h3>👤 Пользователь Telegram:</h3>
+          <p><strong>ID:</strong> {user.id}</p>
+          <p><strong>Имя:</strong> {user.first_name} {user.last_name || ''}</p>
+          {user.username && <p><strong>Username:</strong> @{user.username}</p>}
+        </div>
+      )}
       <nav>
         <Link to="/form-client">Форма клиента</Link>
         <Link to="/form-thrapist-first">Первая форма терапевта</Link>
@@ -40,6 +55,8 @@ function App() {
         <Route path="/form-thrapist-second" element={<TherapistSecondFormComponent />} />
       </Routes>
     </Router>
+
+    
   );
 }
 
