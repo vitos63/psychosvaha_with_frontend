acdbc00d8466f05dffa06a690b3d93b59d187d49
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-01-30 21:20:10 +0300

    ✨ (repo, dto): Create repo and dto for therapists

---
+++ backend/dto/therapist_request.py
@@ -0,0 +1,47 @@
+
+from pydantic import BaseModel, field_validator, Field
+from email_validator import validate_email
+
+from .enums import Sex
+
+
+class CreateTherapist(BaseModel):
+    first_name: str
+    last_name: str
+    city: str | None = None
+    phone_number: str | None = None
+    email: str | None = None
+    photo: str | None = None
+    approved: bool = False
+    consent: bool = False
+    pitch: str | None = None
+    site: str | None = None
+    sex: Sex | None = None
+    age: int = Field(ge=20, le=90)
+    experience: int
+    count_of_recomendations: int = 0
+
+    min_client_age: int = Field(ge=1, le=120)
+    max_client_age: int = Field(ge=2, le=120)
+    online: bool = False
+    currency_amount: dict
+    contacts_for_client: str | None = None
+    psychiatrist: bool = False
+    group_therapy: bool = False
+    supervisor: bool = False
+    gerontologist: bool = False
+    couple_therapist: bool = False
+    available_to_call: bool = False
+
+    @field_validator("email")
+    @classmethod
+    def validate_email_field(cls, email: str) -> str:
+        validate_email(email)
+        return email
+
+    @field_validator("experience")
+    @classmethod
+    def validate_experience_field(cls, experience: int) -> int:
+        if cls.age - experience < 20:
+            raise ValueError("Experience is not valid")
+        return experience
---
+++ backend/repo/therapists.py
@@ -0,0 +1,53 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models import Therapist
+from dto.therapist_request import CreateTherapist
+
+
+class TherapistRepo:
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    async def create_request(self, dto: CreateTherapist) -> Therapist:
+        therapist_request = Therapist(
+            first_name=dto.first_name,
+            last_name=dto.last_name,
+            city=dto.city,
+            phone_number=dto.phone_number,
+            email=dto.email,
+            photo=dto.photo,
+            approved=dto.approved,
+            consent=dto.consent,
+            pitch=dto.pitch,
+            site=dto.site,
+            sex=dto.sex,
+            age=dto.age,
+            city=dto.city,
+            currency_amount=dto.currency_amount,
+            experience=dto.experience,
+            count_of_recomendations=dto.count_of_recomendations,
+            min_client_age=dto.min_client_age,
+            max_client_age=dto.max_client_age,
+            online=dto.online,
+            contacts_for_client=dto.contacts_for_client,
+            psychiatrist=dto.psychiatrist,
+            group_therapy=dto.group_therapy,
+            supervisor=dto.supervisor,
+            gerontologist=dto.gerontologist,
+            couple_therapist=dto.couple_therapist,
+            available_to_call=dto.available_to_call,
+        )
+        self._session.add(therapist_request)
+        await self._session.flush()
+        return therapist_request
+
+    async def select_by_tg_id(self, tg_id: int) -> Therapist | None:
+        stmt = (
+            select(Therapist)
+            .where(
+                Therapist.tg_id == tg_id,
+            )
+        )
+        result = await self._session.execute(stmt)
+        return result.scalars().first()
