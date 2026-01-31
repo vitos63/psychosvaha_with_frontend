ecea1613b7683651a14b1598b51b9eed483b91d6
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-01-31 12:54:03 +0300

    ✨ (service): Add methods for update therapist

--- backend/service/therapist.py
+++ backend/service/therapist.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.models import Therapist
-from dto.therapist import CreateTherapist
+from dto.therapist import CreateTherapist, UpdateTherapist
 from repo.therapists import TherapistRepo
 from repo.therapist_tags import TherapistTagRepo
 
@@ -21,6 +21,18 @@ class TherapistService:
         try:
             therapist = await self._therapist_repo.create_therapist(therapist_dto)
             await self._therapist_tags_repo.create_therapist_tags(therapist_tg_id=therapist.tg_id,
+                                                                  tag_ids=therapist_dto.tag_ids)
+            await self._session.commit()
+            return therapist
+        except Exception:
+            await self._session.rollback()
+            raise
+
+    async def update_therapist(self, therapist_tg_id: int, therapist_dto: UpdateTherapist) -> Therapist:
+        try:
+            therapist = await self._therapist_repo.update_therapist(therapist_tg_id=therapist_tg_id, 
+                                                                    therapist_dto=therapist_dto)
+            await self._therapist_tags_repo.update_therapist_tags(therapist_tg_id=therapist.tg_id,
                                                                   tag_ids=therapist_dto.tag_ids)
             await self._session.commit()
             return therapist
