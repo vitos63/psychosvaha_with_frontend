c652af846b74be27bb934e181fd0ee54332785be
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-02-17 11:32:25 +0300

    🚀 (general): Added configurations for deploy

--- backend/enums/bot_messages.py
+++ backend/enums/bot_messages.py
@@ -1,5 +1,5 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class BotMessages(Enum):
+class BotMessages(StrEnum):
     start_command = "Добро пожаловать, бла бла бла, вот тебе 2 или 3 кнопки"
