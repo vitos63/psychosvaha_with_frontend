115439b39ae4da6f492933728004237c16b79733
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-04-19 17:03:38 +0300

    ✨ (bot): Added real links for bot

--- backend/bot/keyboards/command_keyboards.py
+++ backend/bot/keyboards/command_keyboards.py
@@ -2,13 +2,13 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 
 start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://github.com/"))],
-    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://github.com/vitos63"))],
+    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client"))],
+    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-first"))],
 ])
 
 
 start_keyboard_admin = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Я администратор", callback_data="admin")],
-    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://github.com/"))],
-    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://github.com/vitos63"))],
+    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client"))],
+    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-first"))],
 ])
