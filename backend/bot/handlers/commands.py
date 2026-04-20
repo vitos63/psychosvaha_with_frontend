6ce5f5a12c4fa07c6f27084a3af61244ac9d4312
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-04-20 22:07:23 +0300

    ✨ (bot): Added buttons for start command

--- backend/bot/handlers/commands.py
+++ backend/bot/handlers/commands.py
@@ -6,9 +6,11 @@ from typing import Annotated
 
 from enums.bot_messages import BotMessages
 from service.admin_service import AdminService
-from ..keyboards.command_keyboards import (start_keyboard_admin,
-                                           start_keyboard)
+from service.therapist import TherapistService
+from service.client_request import ClientRequestService
+from ...enums.therapist_statuses import TherapistStatuses
 from modules.di.container import Container
+from keyboards.command_keyboards import CommandKeyboardBuilder
 
 command_router = Router()
 
@@ -16,14 +18,24 @@ command_router = Router()
 @command_router.message(Command("start"))
 @inject
 async def start_handler(message: Message,
-                        admin_service: Annotated[AdminService, Provide[Container.admin_service]]):
+                        admin_service: Annotated[AdminService, Provide[Container.admin_service]],
+                        therapist_service: Annotated[TherapistService, Provide[Container.therapist_service]],
+                        client_request_service: Annotated[ClientRequestService, Provide[Container.client_request_service]]):
     answer = BotMessages.start_command.value
     user_id = message.from_user.id
     admin = await admin_service.check_is_admin(tg_id=user_id)
-    if admin:
-        keyboard = start_keyboard_admin
+    therapist = await therapist_service.get_therapist_by_tg_id(therapist_tg_id=user_id)
+    client_request = await client_request_service.get_client_request_by_tg_id(tg_id=user_id)
+    if therapist:
+        keyboard = CommandKeyboardBuilder(is_admin=bool(admin),
+                                          has_client_request=bool(client_request),
+                                          therapist_status=therapist.status.value,
+                                          tg_id=user_id).get_start_keyboard()
 
     else:
-        keyboard = start_keyboard
+        keyboard = CommandKeyboardBuilder(is_admin=bool(admin),
+                                          has_client_request=bool(client_request),
+                                          therapist_status=TherapistStatuses.NO_QUESTIONARY.value,
+                                          tg_id=user_id).get_start_keyboard()
 
     await message.answer(answer, reply_markup=keyboard)
--- backend/bot/keyboards/command_keyboards.py
+++ backend/bot/keyboards/command_keyboards.py
@@ -1,14 +1,50 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-
-start_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client"))],
-    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-first"))],
-])
+from ...enums.therapist_statuses import TherapistStatuses
 
 
-start_keyboard_admin = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Я администратор", callback_data="admin")],
-    [InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client"))],
-    [InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-first"))],
-])
+class CommandKeyboardBuilder:
+    def __init__(self, is_admin: bool,
+                 has_client_request: bool,
+                 therapist_status: str,
+                 tg_id: int):
+        self.is_admin = is_admin
+        self.has_client_request = has_client_request
+        self.therapist_status = therapist_status
+        self.tg_id = tg_id
+
+    def get_start_keyboard(self) -> InlineKeyboardMarkup:
+        buttons = []
+        buttons = self.__add_admin_button(buttons=buttons)
+        buttons = self.__add_client_button(buttons=buttons)
+        buttons = self.__add_therapist_button(buttons=buttons)
+
+    def __add_admin_button(self, buttons: list) -> list:
+        if self.is_admin:
+            buttons.append([InlineKeyboardButton(text="Я администратор", callback_data="admin")])
+
+        return buttons
+
+    def __add_therapist_button(self, buttons: list) -> list:
+        if self.therapist_status == TherapistStatuses.NO_QUESTIONARY.value:
+            buttons.append([InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-first"))])
+
+        elif self.therapist_status == TherapistStatuses.APPROVED:
+            buttons.append([InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-second"))])
+
+        elif self.therapist_status == TherapistStatuses.NOT_APPROVED:
+            buttons.append([InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url="https://psychosvaha.ru/form-thrapist-success"))])
+
+        elif self.therapist_status == TherapistStatuses.HAVE_QUESTIONARY:
+            buttons.append([InlineKeyboardButton(text="Я специалист", web_app=WebAppInfo(url=f"https://psychosvaha.ru/therapist/{tg_id}"))])
+
+        return buttons
+
+    def __add_client_button(self, buttons: list) -> list:
+        if not self.has_client_request:
+            buttons.append([InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client"))])
+
+        else:
+            buttons.append([InlineKeyboardButton(text="Я ищу специалиста", web_app=WebAppInfo(url="https://psychosvaha.ru/form-client-success"))])
+
+        return buttons
