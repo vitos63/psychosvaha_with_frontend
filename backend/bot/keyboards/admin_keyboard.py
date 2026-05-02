65914243b39670e4e45661b967e47ce207418805
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2026-05-02 10:45:31 +0300

    🐛 (bot): Fixed syntaxis error

--- backend/bot/keyboards/admin_keyboard.py
+++ backend/bot/keyboards/admin_keyboard.py
@@ -3,5 +3,13 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from constants import BASE_URL
 
 
-admin_keyboard = InlineKeyboardMarkup(inline_keyboard=[InlineKeyboardButton(text="Посмотреть",
-                                                                            callback_data=f"{BASE_URL}/admin")])
+admin_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="Посмотреть",
+                callback_data=f"{BASE_URL}/admin"
+            )
+        ]
+    ]
+)
